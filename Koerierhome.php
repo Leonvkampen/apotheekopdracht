cc26136184e370a86b05bbfdf405c76b31714bbf
Leon Kampen <leonvkampen@gmail.com>
2017-10-09 09:45:09 +0200

    test

---
+++ Koerierhome.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head style="">
+    
+      <link rel="stylesheet" type="text/css" href="css/style.css">
+    
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+
+</head>
+
+
+<body>
+    
+    <div>
+    <h1 style="text-align: center;">Koerierhome</h1>
+    </div>
+
+    </div>
+	<div style="text-align:center;">
+
+	 	<button onclick="location.href='./login.php'" type="button" style="width: 250px;"  class="button" > Bestellingen bekijken </button>
+	</div>
+</body>
+<br><br><br>
+<footer>
+    <?php include 'footer.php'; ?>
+</footer>
+</html>
+    
