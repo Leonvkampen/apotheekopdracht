ab9f17ed7aea048606d84c6b20fbecf7b0081d1d
Leon Kampen <leonvkampen@gmail.com>
2017-10-12 13:01:07 +0200

    leveringkoerier

--- bestellen_stap3.php
+++ bestellen_stap3.php
@@ -47,65 +47,75 @@
 			
             ?>
         
-            <p> Medicijn 1 </p>
+            <p> Medicijn 1 & aantal </p>
             <select name="med1" type="med1">
                 <?php echo $option; ?>
            
               </select>
+        <input type="text" name="aantal1" id="aatal1" placeholder="Vul hier aantal in" value= "1" style="width:15%;"/>
             
-            <p> Medicijn 2 </p>
+            <p> Medicijn 2 & aantal</p>
             <select name="med2" type="med2">
                 <?php echo $option; ?>
            
               </select>
+        <input type="text" name="aantal2" id="aatal2" placeholder="Vul hier aantal in" value= "1" style="width:15%;"/>
             
-			<p> Medicijn 3 </p>
+			<p> Medicijn 3 & aantal</p>
             <select name="med3" type="med3">
                 <?php echo $option; ?>
            
               </select>
+        <input type="text" name="aantal3" id="aatal3" placeholder="Vul hier aantal in"  value= "1"style="width:15%;"/>
 			  
-			<p> Medicijn 4 </p>
+			<p> Medicijn 4 & aantal</p>
             <select name="med4" type="med4">
                 <?php echo $option; ?>
            
               </select>
+        <input type="text" name="aantal4" id="aatal4" placeholder="Vul hier aantal in"  value= "1"style="width:15%;"/>
 			  
-			<p> Medicijn 5 </p>
+			<p> Medicijn 5 & aantal</p>
             <select name="med5" type="med5">
                 <?php echo $option; ?>
            
               </select>
+        <input type="text" name="aantal5" id="aatal5" placeholder="Vul hier aantal in"  value= "1" style="width:15%;"/>
 			  
-			<p> Medicijn 6 </p>
+			<p> Medicijn 6 & aantal</p>
             <select name="med6" type="med6">
                 <?php echo $option; ?>
            
               </select>
+        <input type="text" name="aantal6" id="aatal6" placeholder="Vul hier aantal in"  value= "1" style="width:15%;"/>
 			  
-			<p> Medicijn 7 </p>
+			<p> Medicijn 7 & aantal</p>
             <select name="med7" type="med7">
                 <?php echo $option; ?>
            
               </select>
+        <input type="text" name="aantal7" id="aatal7" placeholder="Vul hier aantal in"  value= "1" style="width:15%;"/>
 			  
-			<p> Medicijn 8 </p>
+			<p> Medicijn 8 & aantal</p>
             <select name="med8" type="med8">
                 <?php echo $option; ?>
            
               </select>
+        <input type="text" name="aantal8" id="aatal8" placeholder="Vul hier aantal in"  value= "1" style="width:15%;"/>
 			  
-			<p> Medicijn 9 </p>
+			<p> Medicijn 9 & aantal</p>
             <select name="med9" type="med9">
                 <?php echo $option; ?>
            
               </select>
+        <input type="text" name="aantal9" id="aatal9" placeholder="Vul hier aantal in" value= "1" style="width:15%;"/>
 			  
-			<p> Medicijn 10 </p>
+			<p> Medicijn 10 & aantal</p>
             <select name="med10" type="med10">
                 <?php echo $option; ?>
            
               </select>
+        <input type="text" name="aantal10" id="aatal10" placeholder="Vul hier aantal in" value= "1" style="width:15%;"/>
           
             
 			<br><br><br>
--- leveringen.php
+++ leveringen.php
@@ -1,11 +1,13 @@
 <?php
     session_start();
     include 'db_connect.php';
-	
-	$query = "SELECT * FROM `order`";
-	$woonplaats = "SELECT * FROM `patient`";
-	
-	$query = "SELECT `verzekeringsnummer` FROM `patient` as p, `order` as o WHERE `idOrder` = "
+
+
+	$querypatient = "SELECT o.idOrder, p.verzekeringsnummer, p.Naam, p.adres, p.postcode FROM `order` as o, `patient` as p 
+                     WHERE p.verzekeringsnummer = o.patient";
+
+    $resultpatient = mysqli_query($connection, $querypatient);
+
 
 ?>
 
@@ -16,68 +18,52 @@
 
 </head>
 <body>
-<?php
-    
 
-		
-        
-		
-				  
-		$result = mysqli_query($connection, $query);
-		
-		
-				  
-        
-        
-        
- 
-        
-    
-    
- 
-
-    
-	mysqli_close($connection);
-
-?>
     
        <h1 style="text-align: center;">Leveringen</h1>
   
 
-<table width="300" border="1" cellpadding="1" cellspacing="1" align="center">
+<table width="300" border="1" cellpadding="1" cellspacing="1" align="center"  >
 <tr>
-				<th>Verzekeringsnummer</th>
-				<th>Naam</th>
-                <th></th>
+				<th>orderID</th>
+				<th>Verzkeringsnummer</th>
+                <th>Naam</th>
+                <th>Adres</th>
+                <th align="center">Postcode</th>
+                <th>    
+                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Identiteitsbewijs&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</th>
 			</tr>
 		<?php
-	       while( $row = mysqli_fetch_assoc($result) )
+	       while( $row = mysqli_fetch_assoc($resultpatient) )
 			{
 				echo "<tr>
-						<td>".$row["Verzekeringsnummer"]."</td>
+						<td>".$row["idOrder"]."</td>
+						<td>".$row["verzekeringsnummer"]."</td>
 						<td>".$row["Naam"]."</td>
-						<td>".$row[""]."</td>
-                        <td>
-							<a href='bestellen_stap2_orderaanmaken.php?verzekeringsnummer=".$row["Verzekeringsnummer"]."'/>
-				            <img src='icons/drop.png' alt='Receptaanmaken'>
-							</a>
-                        </td>
+                        <td>".$row["adres"]."</td>
+                        <td>".$row["postcode"]."</td>
+                        <td><br><br> </td>
                         
 					  </tr>";
 			}
 			
     
         
-    
+    mysqli_close($connection);
     
 		?>
     
-    
-   
-     
-    
+    </table>
+    <div style="text-align:center;">
+
+     <br>
+     <button onclick="location.href='./koerierhome.php'" type="button" style="width: 20%;"  class="button"  > Terug </button>
+     <br>
+    </div>
 
 </body>
+    <br>
+    
 <br><br>
 <footer>
 <?php include 'footer.php'; ?>
